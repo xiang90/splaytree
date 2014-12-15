57a4a0023ee27950ed16f5d4a098642bf20ee2d2
Xiang Li <xiang.li@coreos.com>
2014-12-14 22:24:15 -0800

    add splaytree

--- splaytree.go
+++ splaytree.go
@@ -4,3 +4,51 @@ type binaryNode struct {
 	left, right *binaryNode
 	item        Item
 }
+
+type splayTree struct {
+	root *binaryNode
+}
+
+func NewSplayTree() Tree {
+	return &splayTree{}
+}
+
+func (st *splayTree) Get(key Item) {
+	panic("not implemented")
+}
+
+func (st *splayTree) Has(key Item) bool {
+	panic("not implemented")
+}
+
+func (st *splayTree) ReplaceOrInsert(item Item) Item {
+	panic("not implemented")
+}
+
+func (st *splayTree) Delete(item Item) Item {
+	panic("not implemented")
+}
+
+func (st *splayTree) DeleteMin() Item {
+	panic("not implemented")
+}
+
+func (st *splayTree) DeleteMax() Item {
+	panic("not implemented")
+}
+
+func (st *splayTree) Len() int {
+	panic("not implemented")
+}
+
+func (st *splayTree) AscendRange(greaterOrEqual, lessThan Item, iterator ItemIterator) {
+	panic("not implemented")
+}
+
+func (st *splayTree) AscendLessThan(pivot Item, iterator ItemIterator) {
+	panic("not implemented")
+}
+
+func (st *splayTree) AscendGreaterOrEqual(pivot Item, iterator ItemIterator) {
+	panic("not implemented")
+}
